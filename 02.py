11254d528f872a6e7813adf4e7ca0125c0dc3971
Mariel <marielvertz@gmail.com>
2025-07-31 19:24:08 -0600

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,6 @@
+num1= int (input("Ingresa el primer numero a sumar: "))
+num2= int (input("ingresa el segundo numero a sumar: "))
+op = (num1+num2)
+
+
+print ("La suma de los numeros es de: ", op);
